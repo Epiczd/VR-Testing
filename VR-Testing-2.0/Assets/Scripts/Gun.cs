f86e80496ae6fb5db267808dfd8b60cd0d63609e
Epiczd <Nutball37@gmail.com>
2020-02-13 10:41:54 -0500

    Grabbable Objects/Gun Progress

---
+++ VR-Testing-2.0/Assets/Scripts/Gun.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Oculus;
+using UnityEngine.Audio;
+
+public class Gun : MonoBehaviour
+{
+    //Determines if the gun is fired
+    private bool gunFired;
+
+    //The Bullet being fired
+    public GameObject bullet;
+
+    //The Sound Effect of the gun
+    public AudioSource gun;
+
+    //OnStart, the bullet is disabled
+    void Start()
+    {
+        bullet.SetActive(false);
+    }
+
+    /* If the player pressed the right index trigger, they will fire the gun.
+     * When the gun is fired, the bullet will move, and the sound effect will play
+     */
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            gunFired = true;
+            gun.Play();
+            bullet.SetActive(true);
+            
+        }
+
+        if(gunFired == true)
+        {
+            UpdateMovement();
+            Debug.Log("Fired");
+        }
+    }
+
+    // When the player fires the gun, this moves the bullet forward
+    void UpdateMovement()
+    {
+        Vector3 MovementDirection = Vector3.forward;
+        bullet.transform.Translate(MovementDirection * Time.deltaTime);
+    }
+}
