e86af79b20a2354c214e1433e5de8a229e4208e2
Epiczd <Nutball37@gmail.com>
2020-02-20 10:43:49 -0500

    BlockMenu Progress

---
+++ VR-Testing-2.0/Assets/Scripts/Creative/CreateBlock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateBlock : MonoBehaviour
+{
+    //Activates the block menu
+    [SerializeField] private Canvas blockMenu;
+
+    //On Start, the block menu is disabled
+    void Start()
+    {
+        blockMenu.enabled = false;
+    }
+
+    //If the player presses B, they will open the block menu
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.RawButton.B))
+        {
+            blockMenu.enabled = true;
+        }
+    }
+}
