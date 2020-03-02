0063ad6e48f0a878e19c0f1e50c4239bae8318ec
Epiczd <Nutball37@gmail.com>
2020-03-02 10:45:19 -0500

    Today's Progress

--- VR-Testing-2.0/Assets/Scripts/Creative/BlockMenu.cs
+++ VR-Testing-2.0/Assets/Scripts/Creative/BlockMenu.cs
@@ -8,10 +8,14 @@ public class BlockMenu : MonoBehaviour
     //Activates the block menu
     [SerializeField] private GameObject blockMenu;
 
-    //On Start, the block menu is disabled
+    //The Pointer
+    [SerializeField] private GameObject pointer;
+
+    //On Start, the block menu and pointer are disabled
     void Start()
     {
         blockMenu.SetActive(false);
+        pointer.SetActive(false);
     }
 
     //If the player presses B, they will open the block menu
@@ -20,6 +24,7 @@ public class BlockMenu : MonoBehaviour
         if (OVRInput.GetDown(OVRInput.RawButton.B))
         {
             blockMenu.SetActive(true);
+            pointer.SetActive(true);
         }
     }
 }
